eb8034e1fd67de0bcf4f24ff5b36646f9291e286
Olausz <2023211510080@iesp.edu.br>
2025-06-02 22:44:24 -0300

    Correção

--- API/src/main/java/com/example/API/domain/Matricula/MatriculaDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.API.domain.Matricula;
-
-import com.example.API.domain.Enums.StatusMatricula;
-
-
-public record MatriculaDTO(Long alunoId, Long disciplinaId, Double nota1, Double nota2, StatusMatricula statusMatricula) {
-}
